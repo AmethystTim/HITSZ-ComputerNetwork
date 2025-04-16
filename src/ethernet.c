26824540e2051c3a95d0a74d196fad21639d9510
Dinorex <1819539569@qq.com>
2025-04-16 12:08:34 +0800

    feat: Eth协议实现

--- src/ethernet.c
+++ src/ethernet.c
@@ -11,6 +11,16 @@
  */
 void ethernet_in(buf_t *buf) {
     // TO-DO
+    if (buf->len < sizeof(ether_hdr_t)) {
+        return;
+    }
+    // 获取以太网包头
+    ether_hdr_t *hdr = (ether_hdr_t *)buf->data;
+    net_protocol_t protocol = hdr->protocol16;
+    protocol = swap16(protocol);
+    // 移除以太网包头
+    buf_remove_header(buf, sizeof(ether_hdr_t));
+    net_in(buf, protocol, hdr->src);
 }
 /**
  * @brief 处理一个要发送的数据包
@@ -21,6 +31,27 @@ void ethernet_in(buf_t *buf) {
  */
 void ethernet_out(buf_t *buf, const uint8_t *mac, net_protocol_t protocol) {
     // TO-DO
+    if (buf->len < ETHERNET_MIN_TRANSPORT_UNIT) {
+        // 填充以太网头部
+        if (buf_add_padding(buf, ETHERNET_MIN_TRANSPORT_UNIT - buf->len) != 0){
+            printf("buf_add_padding error\n");
+            return;
+        }
+    }
+    // 添加以太网包头
+    if (buf_add_header(buf, sizeof(ether_hdr_t)) != 0){
+        printf("buf_add_header error\n");
+        return;
+    }
+    ether_hdr_t *hdr = (ether_hdr_t *)buf->data;
+    // 设置目标MAC地址
+    memcpy(hdr->dst, mac, NET_MAC_LEN);
+    // 设置源MAC地址
+    memcpy(hdr->src, net_if_mac, NET_MAC_LEN);
+    // 填写协议类型
+    hdr->protocol16 = swap16(protocol);
+    // 发送数据帧
+    driver_send(buf);
 }
 /**
  * @brief 初始化以太网协议
